7fce78ed969385a8ab11d11b566035d8695b7130
Andres-debug <andresf.diaz@unac.edu.co>
2024-09-14 22:24:09 -0500

    Add More Text

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola Mundo</Text>
       <Text>Este emnsaje viene de una rama feature</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        doloribus dolore molestiae numquam suscipit itaque totam obcaecati
+        atque, facilis saepe ut similique inventore ea aperiam voluptates
+        repellendus accusantium aspernatur? Dolore!
+      </Text>
     </View>
   );
 }
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
